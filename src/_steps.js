63756a8236152380cfa6054278991ef66a6f0a52
Sakkar Chowdhury <38989084+SkrChowdhury@users.noreply.github.com>
2022-10-03 11:27:55 +0600

    react router installation

---
+++ src/_steps.js
@@ -0,0 +1,6 @@
+/*
+Step 1: npm i react-router-dom
+Step 2: Create a router use createBrowserRouter
+Step 3: Add ROuterProvider and pass router props
+Step 4: Create some route inside the router
+*/
